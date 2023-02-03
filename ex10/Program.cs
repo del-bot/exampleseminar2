62f468e1650135bcc634ef229eba460392cf0e4a
Delyash <delyash91@mail.ru>
2023-02-03 00:24:32 -0500

    Ex10

---
+++ ex10/Program.cs
@@ -0,0 +1,11 @@
+﻿void ThreeDigitalNumber(int number)
+{
+    int num = number % 100;
+    int result = num / 10;
+    Console.WriteLine(result);
+}
+
+Console.WriteLine("input your three digital number ");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+ThreeDigitalNumber(inputNumber);
