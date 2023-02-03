7dc6909fafa557bd6a64de1b24da87035447b6fb
Delyash <delyash91@mail.ru>
2023-02-03 00:26:32 -0500

    Ex13

---
+++ ex13/Program.cs
@@ -0,0 +1,12 @@
+﻿void ThirdNumber(string num)
+{
+   if(num.Length < 3)
+    Console.WriteLine("No third number");
+    else Console.WriteLine(num[2]);
+}
+
+Console.WriteLine("input your number");
+int number = Convert.ToInt32(Console.ReadLine());
+string InputNumber = Convert.ToString(number);
+
+ThirdNumber(InputNumber);
