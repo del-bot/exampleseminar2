94fdae2e1d926efd944d2174ada89e3b92fcc082
Delyash <delyash91@mail.ru>
2023-02-03 00:27:52 -0500

    Ex15

---
+++ ex15/Program.cs
@@ -0,0 +1,11 @@
+﻿void DayOfTheWeek(int day)
+{
+    if (day == 6 || day == 7)
+        Console.WriteLine("It is a weekend");
+    else Console.WriteLine("It is not a weekend, sorry");
+}
+
+Console.WriteLine("Input your day number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+DayOfTheWeek(number);
